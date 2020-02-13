726fc3e546abcaf419573adc1d530a2a0ee45d02
Vanessa Martinez <vmar76@gmail.com>
2020-02-13 23:51:56 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -12,7 +12,7 @@ def gross_for_director(director_data)
     total += director_data[:movies][index][:worldwide_grosses]
     index += 1
   end 
-  total 
+  director_data
 end
 
 # Write a method that, given an NDS creates a new Hash
