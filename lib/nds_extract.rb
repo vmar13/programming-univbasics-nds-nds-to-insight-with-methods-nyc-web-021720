9ec4a12829329bd824c11f5c260c4a05ebb72f9a
Vanessa Martinez <vmar76@gmail.com>
2020-02-14 00:08:50 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -9,7 +9,7 @@ def gross_for_director(director_data)
   total = 0 
   index = 0 
   while index < director_data[:movies].length do 
-    total += director_data[:movies][index][:worldwide_grosses]
+    total += director_data[:movies][index][:worldwide_gross]
     index += 1
   end 
 total
